beee21f85cfa2279772e0b46dbc77dc9f1bc0ed6
bjhufstetler <bjhufstetler@gmail.com>
2020-02-18 13:48:14 -0500

    added extra @params to LandDist

--- R/LandDist.R
+++ R/LandDist.R
@@ -1,10 +1,13 @@
 #' @title Calculates opportunity costs to insert a node
 #' @description Takes a tour and distance matrix and returns the tour length
 #' 
-#' @param distMatrix A \code{n}x\code{n matrix} where first row is not column headers.
+#' @param dMat A \code{n}x\code{n matrix} where first row is not column headers.
 #' Each cell represents the distance from the row index node to the column index node.
 #' @param tour A \code{numeric} vector with the order that the nodes in the coordMatrix are visited. Must be the same length as the coordMatrix
-#' 
+#' @param p A \code{numeric} that tells the function how many jumpers are being considered
+#' @param nodeCount The \code{numeric} number of nodes in the problem instance
+#' @param jumpers An \code{integer vector} of the jumpers currently in the air
+#' @param node The \code{integer} specific jumper currently under consideration
 
 LandDist <- function(dMat, p, nodeCount, tour, jumpers, node){
   tempPlace <- rep(0, (nodeCount-ceiling(p)-1+node)) # Create an empty vector to store the differences
